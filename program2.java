c004b52cbc340a8e03de50559b73403cdf5ae1ec
harshita-jain-18 <harshitajain0418@gmail.com>
2025-02-08 22:37:51 +0530

    Initial commit

---
+++ program2.java
@@ -0,0 +1,6 @@
+//write a program that prints the following line on the screen along with quotes...
+public class program2 {
+    public static void main(String args[]){
+    System.out.println("\"Can we print '\\' with System.out.println() statement?\" ");
+    }
+}

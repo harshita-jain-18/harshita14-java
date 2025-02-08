4496bc7fda8f818f8f141257776b58a99b2491d3
harshita-jain-18 <harshitajain0418@gmail.com>
2025-02-08 22:39:51 +0530

    assignment 1 completed

---
+++ day1 assignment/program1.java
@@ -0,0 +1,11 @@
+//write a program which prints the following about at least 5person:
+public class program1 {
+    public static void main(String args[]){
+        System.out.println("NAME MAIL-ID EMPLOYEE-CODE PHONE");
+        System.out.println("Harshita harshitajain0418@gmail.com 018 8195389555");
+        System.out.println("Ritika ritika028@gmail.com 019 8177722218");
+        System.out.println("Himanshu himanshusharma777@gmail.com 020 9182466555");
+        System.out.println("Lakshay lakshaygoel026@gmail.com 021 8888442210");
+        System.out.println("Ankita  ankita8899@gmail.com 022 9456899100");
+    }
+}
